1aec1bca98f609cfdca51202de4f319db8c4d845
huyanrong <515020842@qq.com>
2019-10-29 16:54:43 +0800

    配置基础路由

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import {  BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import routes from './router/routes'  
+import NavBar from './components/NavBar/NavBar'
 import './App.css';
+
+
+function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      path={route.path}
+      render={props => (
+        <route.component {...props} routes={route.routes} />
+      )}
+    />
+  );
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
+      <Router>
+        <NavBar/>
+        <Switch>
+          {routes.map((route, i) => (
+            <RouteWithSubRoutes key={i} {...route} />
+          ))}
+        </Switch>
+    </Router>
     );
   }
 }
---
+++ src/router/routes.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import Home from '../pages/Home/Home'
+import Blog from '../pages/Blog/Blog'
+import About from '../pages/About/About'
+  
+const routes = [
+  {
+      path:'/home',
+      component:Home,
+  },
+  {
+      path:'/blog',
+      component:Blog
+  },
+  {
+    path:'/about',
+    component:About,
+  }
+]
+export default routes;
